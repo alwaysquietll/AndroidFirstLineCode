c2aaa664cdb37b111138e5c14d8ff05f0193c292
alwaysquietll <alwaysquietll@gmail.com>
2018-12-30 23:23:43 +0800

    Chapter4 2018/12/30 23:23

---
+++ FragmentBestPractice/app/src/main/java/com/example/jack/fragmentbestpractice/News.java
@@ -0,0 +1,26 @@
+package com.example.jack.fragmentbestpractice;
+
+public class News {
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    private String content;
+
+
+}
