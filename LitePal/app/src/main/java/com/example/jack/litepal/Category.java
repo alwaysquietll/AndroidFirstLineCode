74d63803ea697b4f9e5fc870506eb437d30320ab
alwaysquietll <alwaysquietll@gmail.com>
2019-01-01 21:43:45 +0800

    Chapter6 Data Storage 2019/1/1 21:43

---
+++ LitePal/app/src/main/java/com/example/jack/litepal/Category.java
@@ -0,0 +1,22 @@
+package com.example.jack.litepal;
+
+public class Category {
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    private int id;
+
+    private String categoryName;
+
+    private int categoryCode;
+}
