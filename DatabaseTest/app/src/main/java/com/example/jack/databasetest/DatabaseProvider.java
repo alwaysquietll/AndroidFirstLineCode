aabac825325bd4a4261b542d66422d51ac15a028
alwaysquietll <alwaysquietll@gmail.com>
2019-01-02 19:48:05 +0800

    Chapter7 ContentResolver 2019/1/2 19:48

--- DatabaseTest/app/src/main/java/com/example/jack/databasetest/DatabaseProvider.java
+++ DatabaseTest/app/src/main/java/com/example/jack/databasetest/DatabaseProvider.java
@@ -15,7 +15,7 @@ public class DatabaseProvider extends ContentProvider {
 
     public static final int CATEGORY_DIR = 2;
 
-    public static final int CATEGRORY_ITEM = 3;
+    public static final int CATEGORY_ITEM = 3;
 
     public static final String AUTHORITY = "com.example.jack.databasetest.provider";
 
@@ -28,7 +28,7 @@ public class DatabaseProvider extends ContentProvider {
         uriMatcher.addURI(AUTHORITY, "book", BOOK_DIR);
         uriMatcher.addURI(AUTHORITY, "book/#", BOOK_ITEM);
         uriMatcher.addURI(AUTHORITY, "category", CATEGORY_DIR);
-        uriMatcher.addURI(AUTHORITY, "category/#", CATEGRORY_ITEM);
+        uriMatcher.addURI(AUTHORITY, "category/#", CATEGORY_ITEM);
     }
 
     public DatabaseProvider() {
@@ -36,21 +36,66 @@ public class DatabaseProvider extends ContentProvider {
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        // Implement this to handle requests to delete one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        int deletedRows = 0;
+        switch (uriMatcher.match(uri)) {
+            case BOOK_DIR:
+                deletedRows = db.delete("Book", selection, selectionArgs);
+                break;
+            case BOOK_ITEM:
+                String bookId = uri.getPathSegments().get(1);
+                deletedRows = db.delete("Book", "id = ?", new String[] { bookId });
+                break;
+            case CATEGORY_DIR:
+                deletedRows = db.delete("Category", selection, selectionArgs);
+                break;
+            case CATEGORY_ITEM:
+                String categoryId = uri.getPathSegments().get(1);
+                deletedRows = db.delete("Category", "id = ?", new String[] { categoryId });
+                break;
+            default:
+                break;
+        }
+        return deletedRows;
     }
 
     @Override
     public String getType(Uri uri) {
-        // TODO: Implement this to handle requests for the MIME type of the data
-        // at the given URI.
-        throw new UnsupportedOperationException("Not yet implemented");
+        switch (uriMatcher.match(uri)) {
+            case BOOK_DIR:
+                return "vnd.android.cursor.dir/vnd.com.example.databasetest. provider.book";
+            case BOOK_ITEM:
+                return "vnd.android.cursor.item/vnd.com.example.databasetest. provider.book";
+            case CATEGORY_DIR:
+                return "vnd.android.cursor.dir/vnd.com.example.databasetest. provider.category";
+            case CATEGORY_ITEM:
+                return "vnd.android.cursor.item/vnd.com.example.databasetest. provider.category";
+        }
+        return null;
     }
+
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        Uri uriReturn = null;
+        switch (uriMatcher.match(uri)) {
+            case BOOK_DIR:
+            case BOOK_ITEM:
+                long newBookId = db.insert("Book", null, values);
+                uriReturn = Uri.parse("context://" + AUTHORITY + "/book/" +
+                newBookId);
+                break;
+            case CATEGORY_DIR:
+            case CATEGORY_ITEM:
+                long newCategoryId = db.insert("Category", null, values);
+                uriReturn = Uri.parse("context://" + AUTHORITY + "/category/" +
+                newCategoryId);
+                break;
+            default:
+                break;
+        }
+        return uriReturn;
     }
 
     @Override
@@ -71,6 +116,22 @@ public class DatabaseProvider extends ContentProvider {
                 cursor = db.query("Book", projection, selection, selectionArgs, null,
                         null, sortOrder);
                 break;
+            case BOOK_ITEM:
+                String bookId = uri.getPathSegments().get(1);
+                cursor = db.query("Book", projection, "id = ?", new String[] {bookId},
+                        null, null, sortOrder);
+                break;
+            case CATEGORY_DIR:
+                cursor = db.query("Category", projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            case CATEGORY_ITEM:
+                String categoryId = uri.getPathSegments().get(1);
+                cursor = db.query("Category", projection, "id = ?", new String[]
+                        {categoryId}, null, null, sortOrder);
+                break;
+            default:
+                break;
         }
         return cursor;
     }
@@ -78,7 +139,27 @@ public class DatabaseProvider extends ContentProvider {
     @Override
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
-        // TODO: Implement this to handle requests to update one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        int updatedRows = 0;
+        switch (uriMatcher.match(uri)) {
+            case BOOK_DIR:
+                updatedRows = db.update("Book", values, selection, selectionArgs);
+                break;
+            case BOOK_ITEM:
+                String bookId = uri.getPathSegments().get(1);
+                updatedRows = db.update("Book", values, "id = ?", new String[] { bookId });
+                break;
+            case CATEGORY_DIR:
+                updatedRows = db.update("Category", values, selection, selectionArgs);
+                break;
+            case CATEGORY_ITEM:
+                String categoryId = uri.getPathSegments().get(1);
+                updatedRows = db.update("Category", values, "id = ?", new String[] { categoryId });
+                break;
+            default:
+                break;
+        }
+        return updatedRows;
+
     }
 }
